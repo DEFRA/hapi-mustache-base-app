8581188de06dc7a9e6dd76ce5e6255f1c267e14e
Duncan Nixon <duncannixon@me.com>
2017-08-01 09:54:00 +0100

    Refactored gulp file. Moved 'views' folder to underneath 'src'. Wrote a meaningful unit test using Lab and the Code assertion library. Got the tests to run as a 'gulp test' Gulp task as well as producing an HTML  coverage report.

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,149 +2,143 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
 const standard = require('gulp-standard')
+const lab = require('gulp-lab')
 const runSequence = require('run-sequence')
 const del = require('del')
-const watch = require('gulp-watch')
-const nodemon = require('gulp-nodemon');
+const nodemon = require('gulp-nodemon')
 const browserSync = require('browser-sync')
-const reload = browserSync.reload;
+const reload = browserSync.reload
 
 const paths = {
-    assets: 'src/assets/sass/',
-    public: 'public/',
-    govukModules: 'govuk_modules/'
+  assets: 'src/assets/',
+  govukModules: 'govuk_modules/',
+  public: 'public/',
+  src: 'src/',
+  views: 'src/views/'
 }
 
 gulp.task('clean', () => {
-    return del([paths.public, paths.govukModules])
+  return del([paths.public, paths.govukModules])
 })
-
 
 // Copy govuk files
 
 gulp.task('copy-govuk-toolkit', () => {
-    return gulp.src(['node_modules/govuk_frontend_toolkit/**/*.*'])
-        .pipe(gulp.dest(paths.govukModules + 'govuk_frontend_toolkit/'))
+  return gulp.src(['node_modules/govuk_frontend_toolkit/**/*.*'])
+    .pipe(gulp.dest(paths.govukModules + 'govuk_frontend_toolkit/'))
 })
 
 gulp.task('copy-govuk-template', () => {
-    return gulp.src(['node_modules/govuk_template_mustache/**/*.*'])
-        .pipe(gulp.dest(paths.govukModules + 'govuk_template_mustache/'))
+  return gulp.src(['node_modules/govuk_template_mustache/**/*.*'])
+    .pipe(gulp.dest(paths.govukModules + 'govuk_template_mustache/'))
 })
 
 gulp.task('copy-govuk-elements-sass', () => {
-    return gulp.src(['node_modules/govuk-elements-sass/public/sass/**'])
-        .pipe(gulp.dest(paths.govukModules + '/govuk-elements-sass/'))
+  return gulp.src(['node_modules/govuk-elements-sass/public/sass/**'])
+    .pipe(gulp.dest(paths.govukModules + '/govuk-elements-sass/'))
 })
 
 gulp.task('copy-govuk-files', [], (done) => {
-    runSequence(
-        'copy-govuk-toolkit',
-        'copy-govuk-template',
-        'copy-govuk-elements-sass',
-        done)
+  runSequence(
+    'copy-govuk-toolkit',
+    'copy-govuk-template',
+    'copy-govuk-elements-sass',
+    done)
 })
-
 
 // Install the govuk files into our application
 
 gulp.task('copy-template-assets', () => {
-    gulp
-        .src(paths.govukModules + '/govuk_template_mustache/assets/{images/**/*.*,javascripts/**/*.*,stylesheets/**/*.*}')
-        .pipe(gulp.dest(paths.public))
+  gulp
+    .src(paths.govukModules + '/govuk_template_mustache/assets/{images/**/*.*,javascripts/**/*.*,stylesheets/**/*.*}')
+    .pipe(gulp.dest(paths.public))
 })
 
 gulp.task('copy-template-view', () => {
-    gulp
-        .src('node_modules/govuk_template_mustache/views/**/*.*')
-        .pipe(gulp.dest('views/govuk_template_mustache'))
+  gulp
+    .src('node_modules/govuk_template_mustache/views/**/*.*')
+    .pipe(gulp.dest('src/views/govuk_template_mustache'))
 })
 
 gulp.task('install-govuk-files', [], (done) => {
-    runSequence(
-        'copy-template-assets',
-        'copy-template-view',
-        done)
+  runSequence(
+    'copy-template-assets',
+    'copy-template-view',
+    done)
 })
-
 
 // Build the sass
 gulp.task('sass', () => {
-    return gulp.src('src/assets/' + 'sass/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            outputStyle: 'expanded',
-            includePaths: [
-                'govuk_modules/govuk_frontend_toolkit/stylesheets',
-                'govuk_modules/govuk_template_mustache/assets/stylesheets',
-                'govuk_modules/govuk-elements-sass/'
-            ]
-        }).on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.public + 'stylesheets/'))
-        .pipe(reload({
-            stream: true
-        }));
+  return gulp.src(paths.assets + 'sass/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'expanded',
+      includePaths: [
+        'govuk_modules/govuk_frontend_toolkit/stylesheets',
+        'govuk_modules/govuk_template_mustache/assets/stylesheets',
+        'govuk_modules/govuk-elements-sass/'
+      ]
+    }).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.public + 'stylesheets/'))
+    .pipe(reload({
+      stream: true
+    }))
 })
-
 
 // Run StardardJS checks
 gulp.task('standard', () => {
-    return gulp.src(['src/**/*.js'])
-        .pipe(standard())
-        .pipe(standard.reporter('default', {
-            breakOnError: true,
-            quiet: true
-        }))
+  return gulp.src([paths.src + '**/*.js'])
+    .pipe(standard())
+    .pipe(standard.reporter('default', {
+      breakOnError: true,
+      quiet: true
+    }))
 })
-
 
 // Test task
-gulp.task('test', [], () => {
-    // TODO - run tests
-    gulp.run('standard')
+gulp.task('test', ['standard'], () => {
+  return gulp.src('test')
+    .pipe(lab('--coverage --reporter console --output stdout --reporter html --output coverage.html --verbose'))
 })
-
 
 // Build task
 gulp.task('build', ['clean'], (done) => {
-    runSequence(
-        'copy-govuk-files',
-        'install-govuk-files',
-        'sass',
-        done)
+  runSequence(
+    'copy-govuk-files',
+    'install-govuk-files',
+    'sass',
+    done)
 })
 
 gulp.task('browser-sync', ['nodemon'], () => {
-    browserSync.init({
-        proxy: 'http://localhost:8000',
-        browser: 'google chrome',
-        port: 3000,
-        reloadDelay: 1000
-    })
+  browserSync.init({
+    proxy: 'http://localhost:8000',
+    browser: 'google chrome',
+    port: 3000,
+    reloadDelay: 1000
+  })
 })
 
 gulp.task('nodemon', (cb) => {
+  let started = false
 
-    let started = false
-
-    return nodemon({
-        script: 'index.js',
-        ext: 'js html'
-    }).once('start', () => {
-        // To avoid nodemon being started multiple times
-        if (!started) {
-            started = true
-            cb()
-        }
-    })
+  return nodemon({
+    script: 'index.js',
+    ext: 'js html'
+  }).once('start', () => {
+    // To avoid nodemon being started multiple times
+    if (!started) {
+      started = true
+      cb()
+    }
+  })
 })
 
 gulp.task('watch', () => {
-    gulp.watch('src/assets/sass/**/*.scss', ['sass'])
-    gulp.watch('public/**/*.*').on('change', reload)
-    gulp.watch('views/**/*.*').on('change', reload)
-    gulp.watch('src/**/*.*').on('change', reload);
-});
+  gulp.watch(paths.assets + 'sass/**/*.scss', ['sass'])
+  gulp.watch(paths.public + '**/*.*').on('change', reload)
+  gulp.watch(paths.src + '**/*.*').on('change', reload)
+})
 
 gulp.task('default', ['watch', 'sass', 'browser-sync'])
---
+++ test/example2.js
@@ -0,0 +1,27 @@
+const Lab = require('lab')
+const lab = exports.lab = Lab.script()
+const Code = require('code')
+const server = require('../index')
+
+lab.experiment('Basic HTTP Tests', () => {
+  lab.test('GET /login (endpoint test)', (done) => {
+    var options = {
+      method: 'GET',
+      url: '/login'
+    }
+
+    // Simulate an http request
+    server.inject(options, (response) => {
+      //  Expect http response status code to be 200 ('Ok')
+      Code.expect(response.statusCode).to.equal(200)
+
+      // Expect some content to be on the returned page
+      Code.expect(response.result).to.contain('What\'s the site name?')
+
+      // Expect the content to be of a certain length
+      Code.expect(response.result).to.have.length(5116)
+
+      server.stop(done)  // done() callback is required to end the test.
+    })
+  })
+})
